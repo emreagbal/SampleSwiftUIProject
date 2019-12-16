afd33f0e4f42577f9df0f6ef48d653c6345ea391
Emre Ağbal <emre.agbal@outlook.com>
2019-12-16 07:05:20 +0300

    #Developed Some different design added

---
+++ SampleProject/appView.swift
@@ -0,0 +1,33 @@
+//
+//  discoverView.swift
+//  SampleProject
+//
+//  Created by Emre Ağbal on 16.12.2019.
+//  Copyright © 2019 Emre Ağbal. All rights reserved.
+//
+
+import SwiftUI
+
+struct appView: View {
+    @State var selectedTab = 0
+    var body: some View {
+        TabView(selection: $selectedTab){
+            ContentView()
+                .tabItem {
+                    Image("homeOn")
+            }.tag(0)
+            
+            moviesView()
+                .tabItem {
+                    Image("search")
+            }.tag(1)
+        
+        }
+    }
+}
+
+struct appView_Previews: PreviewProvider {
+    static var previews: some View {
+        appView()
+    }
+}
